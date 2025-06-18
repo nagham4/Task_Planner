41b02af5d337a7a02a468f5e54c49cc04c35bbde
aya-maali <ayamaali80@gmail.com>
2025-06-18 15:07:51 +0300

    checkif tasks are empty

--- script.js
+++ script.js
@@ -384,7 +384,8 @@ scrollContainerDiv.addEventListener("click", async (event)=>{
           if(foundTask){
             let foundTaskIndex = unloadedTasks.indexOf(foundTask);
             storeTaskChange(unloadedTasks[foundTaskIndex], foundTaskIndex, 1);
-             taskToDelete.remove();
+            taskToDelete.remove();
+            noTasksChecker();
           }
         }
       }
