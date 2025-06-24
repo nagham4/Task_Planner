612bc29f5f47fec4b9cb502903398652c7c1a1b0
aya-maali <ayamaali80@gmail.com>
2025-06-24 20:38:59 +0000

    -

--- script.js
+++ script.js
@@ -391,7 +391,7 @@ scrollContainerDiv.addEventListener("click", async (event)=>{
       }
    }
 
-  // Event for editing task
+  
   if(event.target.alt === "pencilIcon" ){
     let flag = await confirmOpreationEdit();
     if(flag[0] === "1"){
