86f321a09aa7b43a6858d3ce3345ec0569429786
SADEEL ARDAH <sadeelardah@gmail.com>
2025-06-24 23:13:31 +0300

    تعديلات

--- script.js
+++ script.js
@@ -1,89 +1,52 @@
-// Edit content Alert buttons and div
+// تم التعديل لتضمين مفاهيم DOM والـ ES6 الحديثة بناءً على الروابط المرسلة
 
-//./icons/pencil-svgrepo-com (1).svg
-//./icons/trash-bin-2-svgrepo-com.svg
+// استخدام const و let بدلاً من var
+const alertEdit = document.getElementById("confirmEdit");
+const confirmOpreationButtonEdit = document.getElementById("confirmOpreationButtonEdit");
+const cancelOpreationButtonEdit = document.getElementById("cancelOpreationButtonEdit");
+const inputEdit = document.getElementById("alertEditContentInput");
+const inputEditNote = document.getElementById("inputEditNote");
 
+const alert = document.getElementById("confirmOpreation");
+const confirmOpreationButton = document.getElementById("confirmOpreationButton");
+const cancelOpreationButton = document.getElementById("cancelOpreationButton");
 
-let alertEdit = document.getElementById("confirmEdit");
-let confirmOpreationButtonEdit = document.getElementById(
-  "confirmOpreationButtonEdit"
-);
-let cancelOpreationButtonEdit = document.getElementById(
-  "cancelOpreationButtonEdit"
-);
-let inputEdit = document.getElementById("alertEditContentInput");
-let inputEditNote = document.getElementById("inputEditNote");
+const mainInput = document.querySelector("#mainInput");
+const addNewTaskButton = document.querySelector("#inputButton");
+const inputNote = document.querySelector("#inputNote");
 
-// Alert buttons and div
-let alert = document.getElementById("confirmOpreation");
-let confirmOpreationButton = document.getElementById("confirmOpreationButton");
-let cancelOpreationButton = document.getElementById("cancelOpreationButton");
+const allButton = document.querySelector("#all");
+const doneButton = document.querySelector("#done");
+const todoButton = document.querySelector("#todo");
 
-//Add new task button and input
-let mainInput = document.getElementById("mainInput");
-let addNewTaskButton = document.getElementById("inputButton");
-let inputNote = document.getElementById("inputNote");
+const scrollContainerDiv = document.querySelector("#scrollContainer");
+const noTasksHeader = document.querySelector("#noTasksHeader");
+const taskArr = document.getElementsByClassName("task");
 
-//Blue button Click
-let allButton = document.getElementById("all");
-let doneButton = document.getElementById("done");
-let todoButton = document.getElementById("todo");
-
-// Scroll container
-let scrollContainerDiv = document.getElementById("scrollContainer");
-
-// No tasks edge case header
-let noTasksHeader = document.getElementById("noTasksHeader");
-
-//Task HTML COLLECTION like an array
-let taskArr = document.getElementsByClassName("task");
-
-//Red button Click
-let deleteDoneButton = document.getElementById("deleteDone");
-let deleteAllButton = document.getElementById("deleteAll");
+const deleteDoneButton = document.querySelector("#deleteDone");
+const deleteAllButton = document.querySelector("#deleteAll");
 
 const noTasksChecker = () => {
-  console.log("Inside noTasksChecker");
-  let taskCount = document.getElementsByClassName("task");
-  if (taskCount.length <= 0) {
-    noTasksHeader.style.display = "block";
-  } else {
-    noTasksHeader.style.display = "";
-  }
+  noTasksHeader.style.display = document.querySelectorAll(".task").length ? "none" : "block";
 };
 
 const removeTasksHTML = () => {
-  let tasks = document.getElementsByClassName("task");
-  Array.from(tasks).forEach((task) => {
-    task.remove();
-  });
+  document.querySelectorAll(".task").forEach(task => task.remove());
 };
 
 const removeTasksDoneHTML = () => {
-  let tasks = document.getElementsByClassName("task");
-  Array.from(tasks).forEach((task) => {
-    if (task.id[1] == "t") task.remove();
+  document.querySelectorAll(".task").forEach(task => {
+    if (task.id[1] === "t") task.remove();
   });
 };
 
 const storeTaskChange = (task, index, flag) => {
   let temp = unloadTask();
-  console.log("inside storeTaskChange");
-
-  /**
-   *  flag = 0 only content change to local storage
-   *  flag = 1 deleting entire object from the array
-   *
-   *
-   */
-
-  if (temp.length > 0 && task != null) {
+  if (temp.length > 0 && task) {
     switch (flag) {
       case 0:
-        temp[index].taskId = task.taskId;
-        temp[index].paragraphContent = task.paragraphContent;
+        Object.assign(temp[index], task);
         break;
-
       case 1:
         temp.splice(index, 1);
         break;
@@ -92,346 +55,189 @@ const storeTaskChange = (task, index, flag) => {
   }
 };
 
-const storeLocal = (task) => {
-  let localTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-
-  // Add the new task to the array
+const storeLocal = task => {
+  const localTasks = JSON.parse(localStorage.getItem("tasks")) || [];
   localTasks.push(task);
-
-  // Save the updated task array back to localStorage
   localStorage.setItem("tasks", JSON.stringify(localTasks));
 };
 
-const saveTask = (text) => {
+const saveTask = text => {
   mainInput.value = "";
-  let task = {
-    taskId: Math.floor(Math.random() * 5) + "f",
+  const task = {
+    taskId: `${Math.floor(Math.random() * 5)}f`,
     paragraphStatus: "",
     paragraphContent: text,
   };
-
   displayTaskOnAddNew(task);
   noTasksChecker();
   storeLocal(task);
 };
 
-const unloadTask = () => {
-  const tasks = localStorage.getItem("tasks");
-  const unloadedTasks = tasks ? JSON.parse(tasks) : [];
-  return unloadedTasks;
-};
+const unloadTask = () => JSON.parse(localStorage.getItem("tasks")) || [];
 
-const checkInpt = (textTT) => {
-  const check = /^(?!\d)/;
-
-  if (textTT.length > 5 && check.test(textTT)) {
-    return true;
-  } else {
-    return false;
-  }
-};
+const checkInpt = textTT => textTT.length > 5 && /^(?!\d)/.test(textTT);
 
 const confirmOpreationEdit = () => {
-  let flag = [];
-  flag[0] = "0";
+  let flag = ["0"];
   inputEdit.value = "";
   alertEdit.style.display = "flex";
-
-  return new Promise((resolve, reject) => {
-    confirmOpreationButtonEdit.addEventListener("click", () => {
+  return new Promise(resolve => {
+    const handleConfirm = () => {
       if (checkInpt(inputEdit.value.trim())) {
-        flag[0] = "1";
-        flag[1] = inputEdit.value.trim();
+        flag = ["1", inputEdit.value.trim()];
         alertEdit.style.display = "none";
         resolve(flag);
       } else {
         inputEditNote.style.display = "block";
-        setTimeout(() => {
-          inputEditNote.style.display = "none";
-        }, 3000);
+        setTimeout(() => inputEditNote.style.display = "none", 3000);
       }
-    });
-
-    cancelOpreationButtonEdit.addEventListener("click", () => {
+    };
+    confirmOpreationButtonEdit.onclick = handleConfirm;
+    cancelOpreationButtonEdit.onclick = () => {
       alertEdit.style.display = "none";
       resolve(flag);
-    });
+    };
   });
 };
+
 const confirmOpreation = () => {
-  let flag = false;
   alert.style.display = "flex";
-
-  /*
-    Here is the promise that forces the compiler to wait for input from a diffrent slower part
-    of the code.
-
-    NOTE:
-    You should declare the function that will deal with promises as ASYNC function
-*/
-
-  return new Promise((resolve, reject) => {
-    confirmOpreationButton.addEventListener("click", () => {
+  return new Promise(resolve => {
+    confirmOpreationButton.onclick = () => {
       alert.style.display = "none";
-      flag = true;
-      resolve(flag);
-    }),
-      cancelOpreationButton.addEventListener("click", () => {
-        alert.style.display = "none";
-        resolve((flag = false));
-      });
-
+      resolve(true);
+    };
+    cancelOpreationButton.onclick = () => {
+      alert.style.display = "none";
+      resolve(false);
+    };
   });
 };
 
-const addNewTask = (text) => {
-  saveTask(text);
+const addNewTask = text => saveTask(text);
+
+const createIconButton = (src, alt) => {
+  const img = document.createElement("img");
+  img.src = src;
+  img.alt = alt;
+  return img;
 };
 
-//               NOT FINISHED
-const displayTaskOnAddNew = (task) => {
-  let newTask = document.createElement("div");
-
-  newTask.classList = "task";
+const displayTaskOnAddNew = task => {
+  const newTask = document.createElement("div");
+  newTask.classList.add("task");
   newTask.id = task.taskId;
 
-  //Creating the paragraph to containt the text content
-  let tempParagraph = document.createElement("p");
+  const tempParagraph = document.createElement("p");
   tempParagraph.textContent = task.paragraphContent;
 
-  //Creating Icondiv and content for the icon div
-  let iconDiv = document.createElement("div");
-  iconDiv.classList = "icons";
+  const iconDiv = document.createElement("div");
+  iconDiv.classList.add("icons");
 
-  let tempCheckbox = document.createElement("input");
+  const tempCheckbox = document.createElement("input");
   tempCheckbox.type = "checkbox";
 
-  let tempImg1 = document.createElement("img");
-  tempImg1.src = "./icons/pencil-svgrepo-com.svg";
-  tempImg1.alt = "pencilIcon";
+  const tempImg1 = createIconButton("./icons/pencil-svgrepo-com.svg", "pencilIcon");
+  const tempImg2 = createIconButton("./icons/trash-bin-2-svgrepo-com.svg", "deleteIcon");
 
-  let tempImg2 = document.createElement("img");
-  tempImg2.src = "./icons/trash-bin-2-svgrepo-com.svg";
-  tempImg2.alt = "deleteIcon";
+  [tempCheckbox, tempImg1, tempImg2].forEach(icon => iconDiv.append(icon));
+  [tempParagraph, iconDiv].forEach(el => newTask.append(el));
 
-  //Appending content to the icondiv
-  iconDiv.append(tempCheckbox);
-  iconDiv.append(tempImg1);
-  iconDiv.append(tempImg2);
-
-  //Appending content to the task div
-  newTask.append(tempParagraph);
-  newTask.append(iconDiv);
-
-  //Appending task to task scroll container
   scrollContainerDiv.append(newTask);
 };
 
 const displayTasks = () => {
-  //creating the new div
-  const unloadedTasks = unloadTask();
-
-  if (unloadedTasks.length > 0) {
-    unloadedTasks.forEach((task) => {
-      let newTask = document.createElement("div");
-
-      newTask.classList = "task";
-      newTask.id = task.taskId;
-
-      //Creating the paragraph to containt the text content
-      let tempParagraph = document.createElement("p");
-      tempParagraph.textContent = task.paragraphContent;
-      if (task.taskId[1] === "t")
-        tempParagraph.classList = "taskParagraphCrossed";
-
-      //Creating Icondiv and content for the icon div
-      let iconDiv = document.createElement("div");
-      iconDiv.classList = "icons";
-
-      let tempCheckbox = document.createElement("input");
-      tempCheckbox.type = "checkbox";
-      if (task.taskId[1] === "t") tempCheckbox.checked = true;
-
-      let tempImg1 = document.createElement("img");
-      tempImg1.src = "./icons/pencil-svgrepo-com.svg";
-      tempImg1.alt = "pencilIcon";
-
-      let tempImg2 = document.createElement("img");
-      tempImg2.src = "./icons/trash-bin-2-svgrepo-com.svg";
-      tempImg2.alt = "deleteIcon";
-
-      //Appending content to the icondiv
-      iconDiv.append(tempCheckbox);
-      iconDiv.append(tempImg1);
-      iconDiv.append(tempImg2);
-
-      //Appending content to the task div
-      newTask.append(tempParagraph);
-      newTask.append(iconDiv);
-
-      //Appending task to task scroll container
-      scrollContainerDiv.append(newTask);
-    });
-  }
-  console.log("This is inside display tasks and these are the tasks :");
-  console.log(unloadedTasks);
-  //To make sure the no task note isnt shown
+  unloadTask().forEach(task => displayTaskOnAddNew(task));
 };
+
 addNewTaskButton.onclick = () => {
-  let text = mainInput.value.trim();
+  const text = mainInput.value.trim();
   if (checkInpt(text)) {
     addNewTask(text);
-    inputNote.style.display = "";
+    inputNote.style.display = "none";
   } else {
     inputNote.style.display = "block";
-    setTimeout(() => {
-      inputNote.style.display = "";
-    }, 10000);
-  }
-};
-allButton.onclick = () => {
-  /* Style*/
-  allButton.classList.add("hoverEffect");
-  doneButton.classList.remove("hoverEffect");
-  todoButton.classList.remove("hoverEffect");
-
-  if (taskArr.length >= 1) {
-    for (let task of taskArr) {
-      task.style.display = "";
-    }
-  }
-};
-doneButton.onclick = () => {
-  /* Style*/
-  doneButton.classList.add("hoverEffect");
-  allButton.classList.remove("hoverEffect");
-  todoButton.classList.remove("hoverEffect");
-
-  if (taskArr.length >= 1) {
-    for (let task of taskArr) {
-      if (task.id[1] === "t") task.style.display = "";
-      else task.style.display = "none";
-    }
-  }
-};
-todoButton.onclick = () => {
-  /* Style*/
-  todoButton.classList.add("hoverEffect");
-  allButton.classList.remove("hoverEffect");
-  doneButton.classList.remove("hoverEffect");
-
-  if (taskArr.length >= 1) {
-    for (let task of taskArr) {
-      if (task.id[1] === "f") task.style.display = "";
-      else task.style.display = "none";
-    }
+    setTimeout(() => inputNote.style.display = "none", 3000);
   }
 };
 
-/*
-    I used here Async to wait for the user to input from the buttons
-    as the compiler wont wait for input, either you use timeout callback (Which is hell)
-    or you use promise which is the sane easier choice 
+const setFilter = (filterType) => {
+  [allButton, doneButton, todoButton].forEach(btn => btn.classList.remove("hoverEffect"));
+  const taskList = Array.from(taskArr);
+  switch (filterType) {
+    case "all":
+      allButton.classList.add("hoverEffect");
+      taskList.forEach(task => task.style.display = "");
+      break;
+    case "done":
+      doneButton.classList.add("hoverEffect");
+      taskList.forEach(task => task.style.display = task.id[1] === "t" ? "" : "none");
+      break;
+    case "todo":
+      todoButton.classList.add("hoverEffect");
+      taskList.forEach(task => task.style.display = task.id[1] === "f" ? "" : "none");
+      break;
+  }
+};
 
-    NOTE:
-    You should declare the function that will deal with promises as ASYNC function
-*/
+allButton.onclick = () => setFilter("all");
+doneButton.onclick = () => setFilter("done");
+todoButton.onclick = () => setFilter("todo");
+
 deleteDoneButton.onclick = async () => {
   const unloadedTasks = unloadTask();
-
-  if (unloadedTasks.length >= 1) {
-    let flag = await confirmOpreation();
-
+  if (unloadedTasks.length) {
+    const flag = await confirmOpreation();
     if (flag) {
-      let remainingTasks = unloadedTasks.filter(task => task.taskId[1] !== "t");
-      localStorage.setItem("tasks", JSON.stringify(remainingTasks));
+      const remaining = unloadedTasks.filter(({ taskId }) => taskId[1] !== "t");
+      localStorage.setItem("tasks", JSON.stringify(remaining));
       removeTasksDoneHTML();
       noTasksChecker();
     }
   }
 };
 
-/*aya*/
-deleteAllButton.onclick = async ()=>
-{
-    const unloadedTasks = unloadTask();
-    if(unloadedTasks.length >= 1)
-    {
-      let flag =await confirmOpreation();
-      if(flag){
-        localStorage.removeItem("tasks");
-        displayTasks();
-        removeTasksHTML();
-        noTasksChecker();
-      } 
-    }
-};
-///////                Tasks functions 
-scrollContainerDiv.addEventListener("click", async (event)=>{
-   // Event for deleting task
-   if(event.target.alt === "deleteIcon"){
-      const taskToDelete = event.target.closest(".task");
-      if(taskToDelete){
-        unloadedTasks = unloadTask();
-        if(unloadedTasks.length > 0){
-          let foundTask = unloadedTasks.find(task => taskToDelete.id === task.taskId);
-          if(foundTask){
-            let foundTaskIndex = unloadedTasks.indexOf(foundTask);
-            storeTaskChange(unloadedTasks[foundTaskIndex], foundTaskIndex, 1);
-            taskToDelete.remove();
-            noTasksChecker();
-          }
-        }
-      }
-   }
-
-  // Event for editing task
-  if(event.target.alt === "pencilIcon" ){
-    let flag = await confirmOpreationEdit();
-    if(flag[0] === "1"){
-      const taskToEdit = event.target.closest(".task");
-      const paragraphToEdit  = taskToEdit.querySelector("p");
-      paragraphToEdit.textContent = flag[1];
-      if(taskToEdit){
-        unloadedTasks = unloadTask();
-        if(unloadedTasks.length > 0){
-          let foundTask = unloadedTasks.find(task => taskToEdit.id === task.taskId);
-          if(foundTask){
-            let foundTaskIndex = unloadedTasks.indexOf(foundTask);
-             unloadedTasks[foundTaskIndex].paragraphContent = flag[1];
-             storeTaskChange(unloadedTasks[foundTaskIndex], foundTaskIndex, 0);
-            }
-        }
-
-      }
+deleteAllButton.onclick = async () => {
+  const unloadedTasks = unloadTask();
+  if (unloadedTasks.length) {
+    const flag = await confirmOpreation();
+    if (flag) {
+      localStorage.removeItem("tasks");
+      removeTasksHTML();
+      noTasksChecker();
     }
   }
-  // Event for marking task done
-  if(event.target.type ==="checkbox" && (event.target.checked || !event.target.checked) ){
-    const taskToDone = event.target.closest(".task");
-    const paragraphToEdit  = taskToDone.querySelector("p");
-    if(taskToDone){
-      unloadedTasks = unloadTask();
-      if(unloadedTasks.length > 0){
-        let foundTask = unloadedTasks.find(task => taskToDone.id === task.taskId);
-        if(foundTask){
-          if(taskToDone.id[1] === 'f'){
-             taskToDone.id = taskToDone.id[0] + 't' + taskToDone.id[1].slice(2);
-          }
-          else if (taskToDone.id[1] === 't') {
-             taskToDone.id = taskToDone.id[0] + 'f' + taskToDone.id[1].slice(2);
-          }
-          let foundTaskIndex = unloadedTasks.indexOf(foundTask);
-          unloadedTasks[foundTaskIndex].taskId = taskToDone.id;
-          storeTaskChange(unloadedTasks[foundTaskIndex], foundTaskIndex, 0);
-          paragraphToEdit.classList.toggle ("taskParagraphCrossed");
-        }
-      }
+};
+
+scrollContainerDiv.addEventListener("click", async event => {
+  const { target } = event;
+  if (!target.closest(".task")) return;
+  const taskEl = target.closest(".task");
+  const taskId = taskEl.id;
+  let tasks = unloadTask();
+  const taskIndex = tasks.findIndex(task => task.taskId === taskId);
+  if (target.alt === "deleteIcon") {
+    storeTaskChange(tasks[taskIndex], taskIndex, 1);
+    taskEl.remove();
+    noTasksChecker();
+  } else if (target.alt === "pencilIcon") {
+    const flag = await confirmOpreationEdit();
+    if (flag[0] === "1") {
+      taskEl.querySelector("p").textContent = flag[1];
+      tasks[taskIndex].paragraphContent = flag[1];
+      storeTaskChange(tasks[taskIndex], taskIndex, 0);
     }
+  } else if (target.type === "checkbox") {
+    const p = taskEl.querySelector("p");
+    taskEl.id = taskEl.id[0] + (taskEl.id[1] === "f" ? "t" : "f") + taskEl.id.slice(2);
+    tasks[taskIndex].taskId = taskEl.id;
+    p.classList.toggle("taskParagraphCrossed");
+    storeTaskChange(tasks[taskIndex], taskIndex, 0);
   }
 });
-window.onload = ()=>{
+
+window.onload = () => {
   allButton.classList.add("hoverEffect");
   displayTasks();
   noTasksChecker();
-}
+};
